cc2f98a4ace12bc062d92fec75878c947802502a
debi_prasad_9566 <146007127+debiprasad275@users.noreply.github.com>
2025-02-13 18:06:20 +0530

    Add files via upload

---
+++ builtin_Stackclass.java
@@ -0,0 +1,26 @@
+import java.util.*;
+//In single thread environment we use ArrayDeque instead of stack class
+//cause Stack needs threads
+// aslo both the class have all the functioality like push,peekand pop in java collections
+
+
+public class builtin_Stackclass{
+    public static void main(String[] args) {
+
+        //Stack<Integer> stack = new Stack<Integer>();
+
+        ArrayDeque<Integer> stack = new ArrayDeque<Integer>();
+        // ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        stack.push(10);
+        stack.push(23);
+        stack.push(12);
+
+        System.out.println(stack.peek());   //only return 
+        System.out.println(stack.pop());  //return and remove
+        System.out.println(stack.peek());
+        System.out.println(stack.isEmpty());
+        System.out.println(stack.size());
+
+    }
+}
