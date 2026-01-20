8bb16ff78a26eb77df3d91b6ab3321ccdfc36e97
debi_prasad_9566 <146007127+debiprasad275@users.noreply.github.com>
2026-01-20 22:19:04 +0530

    Add files via upload

---
+++ BScnt1s.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class BScnt1s{
+    public static void main(String[] args) {
+        
+        ArrayList<ArrayList<Integer>> matrix = new ArrayList<>();
+        matrix.add(new ArrayList<> (Arrays.asList(1,1,0,1)));
+        matrix.add(new ArrayList<> (Arrays.asList(1,1,0,1)));
+        matrix.add(new ArrayList<> (Arrays.asList(0,0,0,1)));
+    int n = 3;
+    int m = 3;
+
+
+
+    }
+
+    static int lowerbound(ArrayList<ArrayList<Integer>> matrix,int n,int x){
+
+        int low
+        
+    }
+}
