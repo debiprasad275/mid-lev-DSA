a84d3e5e19fa230c4bfb73621408baf062246e24
debi_prasad_9566 <146007127+debiprasad275@users.noreply.github.com>
2026-02-09 08:34:44 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,14 @@
+public class sum {
+    public static void main(String[] args) {
+        
+        int arr [] ={3,2,1,4};
+        int n=arr.length ;
+        int sum=0;
+        for(int i=0; i<n; i++) {
+           // int sum=0;
+            sum =sum+arr[i];
+
+        }
+    System.out.println("sum is" +sum);
+}
+}
