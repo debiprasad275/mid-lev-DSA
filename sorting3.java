f07157644bb9862f22a60c736ec4a0ba9e52f6d4
debi_prasad_9566 <146007127+debiprasad275@users.noreply.github.com>
2026-02-11 08:27:33 +0530

    Add files via upload

---
+++ sorting3.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+//import javax.sql.rowset.spi.SyncResolver;
+public class sorting3 {
+    public static void main(String[] args) {
+        
+         int [] arr={67,99,98,56,23};
+
+         Arrays.sort(arr,1,3);
+    
+ System.out.println(Arrays.toString(arr));
+}
+}
