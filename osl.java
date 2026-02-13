3c683b27b9ed5c6d640124ad2841bc36e92b9e22
debi_prasad_9566 <146007127+debiprasad275@users.noreply.github.com>
2026-02-13 18:49:31 +0530

    Add files via upload

---
+++ osl.java
@@ -0,0 +1,10 @@
+public class osl {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=0; i<n; i++) {
+            for(int j=2; j<n; j++) {
+                System.out.println("hello");
+    }
+}
+    }
+}
